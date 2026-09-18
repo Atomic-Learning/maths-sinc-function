e37e05f370629d9ccad52d97d41adc09386ad899
Chris Cooling <cmcooling@gmail.com>
2026-09-18 12:49:16 +0100

    Added envelope functions

--- content.md
+++ content.md
@@ -6,7 +6,7 @@ $$
 
 It has the property that $\text{sinc}(0) = 1$.
 
-![A plot of the sinc function](resources/sinc_plot.png)
+![A plot of the sinc function and the functions 1/(πx) and -1/(πx) which act as envelopes for the sinc function](resources/sinc_plot.png)
 
 # Usage
 
--- resources/sinc_plot.py
+++ resources/sinc_plot.py
@@ -4,10 +4,21 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 1000)
 y = scipy.special.sinc(x)
+y_pos_asymptote = 1 / (np.pi * x)
+y_neg_asymptote = -1 / (np.pi * x)
 
-plt.plot(x, y)
+# Break the asymptote curves near x=0 to avoid a vertical connection line.
+near_zero = np.abs(x) < 0.2
+y_pos_asymptote[near_zero] = np.nan
+y_neg_asymptote[near_zero] = np.nan
+
+plt.plot(x, y, label="sinc(x)")
+plt.plot(x, y_pos_asymptote, label="1/(pi x)", linestyle=":", color="tab:orange", alpha=0.55)
+plt.plot(x, y_neg_asymptote, label="-1/(pi x)", linestyle=":", color="tab:green", alpha=0.55)
 plt.title("Sinc Function")
 plt.xlabel("x")
 plt.ylabel("sinc(x)")
+plt.ylim(-0.25, 1.1)
 plt.grid(True)
+plt.legend()
 plt.savefig("resources/sinc_plot.png")
